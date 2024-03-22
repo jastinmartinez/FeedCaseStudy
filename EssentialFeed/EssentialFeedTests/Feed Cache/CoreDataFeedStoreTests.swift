15b19dc683cf9da45baaf4a1842a8324e27f026b
Jastin Martinez <martinez.jastin@hotmail.com>
2024-03-22 18:02:54 -0400

    make all test throwable

--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -12,74 +12,74 @@ import EssentialFeed
 final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     
     
-    func test_retrieve_deliversEmptyOnEmptyCache() {
-        let sut = makeSUT()
+    func test_retrieve_deliversEmptyOnEmptyCache() throws {
+        let sut = try makeSUT()
         
         assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
     }
     
-    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
-        let sut = makeSUT()
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() throws {
+        let sut = try makeSUT()
         
         assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
     }
     
-    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
-        let sut = makeSUT()
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() throws {
+        let sut = try makeSUT()
         
         assertThatRetrieveDeliversFoundValuesOnNonEmptyCache(on: sut)
     }
     
-    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
-        let sut = makeSUT()
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() throws {
+        let sut = try makeSUT()
         
         assertThatRetrieveHasNoSideEffectsOnNonEmptyCache(on: sut)
     }
     
-    func test_insert_deliversNoErrorOnEmptyCache() {
-        let sut = makeSUT()
+    func test_insert_deliversNoErrorOnEmptyCache() throws {
+        let sut = try makeSUT()
         
         assertThatInsertDeliversNoErrorOnEmptyCache(on: sut)
     }
     
-    func test_insert_deliversNoErrorOnNonEmptyCache() {
-        let sut = makeSUT()
+    func test_insert_deliversNoErrorOnNonEmptyCache() throws {
+        let sut = try makeSUT()
         
         assertThatInsertDeliversNoErrorOnNonEmptyCache(on: sut)
     }
     
-    func test_insert_overridesPreviouslyInsertedCacheValues() {
-        let sut = makeSUT()
+    func test_insert_overridesPreviouslyInsertedCacheValues() throws {
+        let sut = try makeSUT()
         
         assertThatInsertOverridesPreviouslyInsertedCacheValues(on: sut)
     }
     
-    func test_delete_deliversNoErrorOnEmptyCache() {
-       let sut = makeSUT()
+    func test_delete_deliversNoErrorOnEmptyCache() throws {
+       let sut = try makeSUT()
         
         assertThatDeleteDeliversNoErrorOnEmptyCache(on: sut)
     }
     
-    func test_delete_hasNoSideEffectsOnEmptyCache() {
-        let sut = makeSUT()
+    func test_delete_hasNoSideEffectsOnEmptyCache() throws {
+        let sut = try makeSUT()
         
         assertThatDeleteHasNoSideEffectsOnEmptyCache(on: sut)
     }
     
-    func test_delete_deliversNoErrorOnNonEmptyCache() {
-        let sut = makeSUT()
+    func test_delete_deliversNoErrorOnNonEmptyCache() throws {
+        let sut = try makeSUT()
         
         assertThatDeleteDeliversNoErrorOnNonEmptyCache(on: sut)
     }
     
-    func test_delete_emptiesPreviouslyInsertedCache() {
-        let sut = makeSUT()
+    func test_delete_emptiesPreviouslyInsertedCache() throws {
+        let sut = try makeSUT()
         
         assertThatDeleteEmptiesPreviouslyInsertedCache(on: sut)
     }
     
-    func test_storeSideEffects_runSerially() {
-        let sut = makeSUT()
+    func test_storeSideEffects_runSerially() throws {
+        let sut = try makeSUT()
         
         assertThatSideEffectsRunSerially(on: sut)
     }
@@ -87,10 +87,10 @@ final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
 // MARK: HELPERS
     
     private func makeSUT(file: StaticString = #file,
-                         line: UInt = #line) -> CoreDataFeedStore {
+                         line: UInt = #line) throws -> CoreDataFeedStore {
         let storeBundle = Bundle(for: CoreDataFeedStore.self)
-        let storerURL = URL(fileURLWithPath: "/dev/null")
-        let sut = try! CoreDataFeedStore(storeURL: storerURL, bundle: storeBundle)
+        let storerURL = URL(fileURLWithPath: "/dev/null") // imaginary memory allocation for CoreData object.
+        let sut = try CoreDataFeedStore(storeURL: storerURL, bundle: storeBundle)
         trackForMemoryLeaks(instance: sut, file: file, line: line)
         return sut
     }
